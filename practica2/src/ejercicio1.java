80564564922db80a0839a998488c3aa20bce3aa4
Jose Manuel <josemanuelmartinez.nene@gmail.com>
2025-10-06 14:24:28 +0200

    ejercicio 1

--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ejercicio1 {
     public static void main(String[] args){
-
+        System.out.println("test");
+        // Crea una array de 10 posiciones de números pedidos por teclado
+        Scanner scanner = new Scanner(System.in);
+        int[] arrayint = new int[10];
+        System.out.println("introduce un valor");
+        int i;
+        int valor = 0;
+        for (i = 0; i < 10; i++){
+            valor = scanner.nextInt();
+            System.out.println("Introduce otro valor");
+        }
+        }
     }
-}
