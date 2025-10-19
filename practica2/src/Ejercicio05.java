16a19a249381e25648b11236ea3a5a073c928761
Kama <josemanuelmartinez.nene@gmail.com>
2025-10-19 18:31:31 +0200

    Ejercicio 1 - 5

---
+++ practica2/src/Ejercicio05.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ejercicio05 {
+    public static void main(String[] args) {
+
+        //Crea un array de números donde le indicamos por teclado el tamaño del array,
+        //rellenaremos el array con números aleatorios entre 0 y 9, al final muestra por pantalla el
+        //valor de cada posición y la suma de todos los valores
+
+        //Inicializamos un nuevo Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Le pedimos al usuario que introduzca el tamaño del array
+        System.out.println(" Indíqueme el tamaño del array");
+        int arrayLength = sc.nextInt();
+
+        //Inicializamos el array
+        int[] arrayInt = new int[arrayLength];
+
+        //Rellenamos el array con números aleatorios entre 1 - 9
+        for (int i = 0; i < arrayInt.length; i++) {
+            arrayInt[i] = (int) (Math.random() * 9 + 1);
+        }
+
+        //Recorremos el array y mostramos por pantalla la posicion y el valor
+        for (int a = 0; a < arrayInt.length; a++) {
+            System.out.println("La posicion es " + a + " El valor es " + arrayInt[a]);
+        }
+
+        //Inicializamos sumNumArray ( para sumar los valores )
+        int sumNumArray = 0;
+
+        //Recorremos el array sumando todos los valores
+        for(int b = 0; b < arrayInt.length; b++){
+            sumNumArray += arrayInt[b];
+        }
+
+        //Introducimos por pantalla la suma de los valores
+        System.out.println(" La suma de los valores es " + sumNumArray);
+    }
+}
--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -6,18 +6,9 @@ public class ejercicio1 {
         // Crea una array de 10 posiciones de números pedidos por teclado
         Scanner scanner = new Scanner(System.in);
         int[] arrayint = new int[10];
-        // le das una longitud a la variable
-        System.out.println("introduzca un valor");
         for (int i = 0; i < arrayint.length; i++){
-            // Recorre la array de 1 en 1 hasta llegar a la longitud de la array
+            System.out.println("introduce un valor");
             arrayint[i] = scanner.nextInt();
-            System.out.println("Introduzca otro valor");
-            // le pides que introduca un número
         }
-        for (int i = 0; i < arrayint.length; i++) {
-            // vuelves a recorrer la array
-            System.out.println("indice "+i+" valor: "+ arrayint[i]);
-            // imprime por pantalla en el indice el valor
         }
     }
-}
