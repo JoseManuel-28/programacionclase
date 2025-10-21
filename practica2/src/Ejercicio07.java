e95f6ff04cdf955980012e0fafbf56a649c03907
Kama <josemanuelmartinez.nene@gmail.com>
2025-10-21 17:39:43 +0200

    cambios Ejercicio07

--- practica2/src/Ejercicio07.java
+++ practica2/src/Ejercicio07.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Ejercicio07 {
     public static void main(String[] args){
         //Programa Java que llene un array con 10 números enteros que se leen por teclado.
         // A continuación calcula y muestra la media de los valores positivos
         // y la dae los valores negativos del array.
+
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[10];
+        int positivos = 0, negativos = 0;
+        int sumNumArray = 0;
+
+        for (int i = 0; i<arrayInt.length; i++){
+            System.out.println("indiqueme 10 números para rellenar el array");
+            arrayInt[i] = sc.nextInt();
+        }
+        for (int a = 0; a < arrayInt.length ; a++) {
+            sumNumArray += arrayInt[a];
+        }
     }
 }
