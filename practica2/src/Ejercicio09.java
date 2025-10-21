10273647c4cb1757abb86be01f4d9cbe07975f0c
Kama <josemanuelmartinez.nene@gmail.com>
2025-10-21 23:39:32 +0200

    ejercicios 07,08,09 completo( añadir comentarios )

--- practica2/src/Ejercicio09.java
+++ practica2/src/Ejercicio09.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ejercicio09 {
     public static void main(String[] args){
 
@@ -7,5 +9,45 @@ public class Ejercicio09 {
         //El valor máximo y la posición en la que aparece.
         //El valor mínimo y la posición en la que aparece.
         //Muestre el resultado en consola, por ejemplo:
+
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[10];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.println("Introduzca 1 número entero ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        int maxNumArray = arrayInt[0];
+
+        for (int a = 0; a < arrayInt.length; a++) {
+            if(arrayInt[a] > maxNumArray){
+                maxNumArray = arrayInt[a];
+            }
+        }
+
+        int minNumArray = arrayInt[0];
+
+        for (int b = 0; b < arrayInt.length; b++) {
+            if(arrayInt[b] < minNumArray){
+                minNumArray = arrayInt[b];
+            }
+        }
+
+        int indexMaxNumArray = -1;
+        int indexMinNumArray = -1;
+
+
+        for (int c = 0; c < arrayInt.length; c++) {
+            if(maxNumArray == arrayInt[c]){
+                indexMaxNumArray = c;
+            }
+            if(minNumArray == arrayInt[c]){
+                indexMinNumArray = c;
+            }
+        }
+
+        System.out.println("Valor máximo: " + maxNumArray + " en la posicíon " + indexMaxNumArray);
+        System.out.println("Valor mínimo: " + minNumArray + " en la posicíon " + indexMinNumArray);
     }
 }
