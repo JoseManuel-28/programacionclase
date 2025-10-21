ad1a8e274ec67d9b0b6bdb5e0066105378a3d0b2
Kama <josemanuelmartinez.nene@gmail.com>
2025-10-21 23:39:32 +0200

    ejercicios 07,08,09 completo( añadir comentarios )

--- practica2/src/Ejercicio07.java
+++ practica2/src/Ejercicio07.java
@@ -4,23 +4,33 @@ public class Ejercicio07 {
     public static void main(String[] args) {
         //Programa Java que llene un array con 10 números enteros que se leen por teclado.
         // A continuación calcula y muestra la media de los valores positivos
-        // y la dae los valores negativos del array.
+        // y la de los valores negativos del array.
 
         Scanner sc = new Scanner(System.in);
         int[] arrayInt = new int[10];
-        int positivos = 0, negativos = 0;
-        int sumNumArray = 0;
+        int totalPositive = 0;
+        int totalNegative = 0;
 
         for (int i = 0; i < arrayInt.length; i++) {
-            System.out.println("indiqueme 10 números para rellenar el array");
+            System.out.println("indiqueme un número positivo o negativo para rellenar el array");
             arrayInt[i] = sc.nextInt();
         }
+
         for (int a = 0; a < arrayInt.length; a++) {
             if (arrayInt[a] > 0) {
-                positivos++;
+                totalPositive += arrayInt[a];
             } else if (arrayInt[a] < 0) {
-                negativos++;
+                totalNegative += arrayInt[a];
             }
         }
+
+        System.out.println("La suma de los números positivos son " + totalPositive);
+        System.out.println("La suma de los números negativos son " + totalNegative);
+
+        int averagePositive = totalPositive/arrayInt.length;
+        int averageNegative = totalNegative/arrayInt.length;
+
+        System.out.println("La media de los números positivos es " + averagePositive);
+        System.out.println("La media de los números negativos es " + averageNegative);
     }
 }
--- practica2/src/Ejercicio08.java
+++ practica2/src/Ejercicio08.java
@@ -7,12 +7,37 @@ public class Ejercicio08 {
         // una altura superior a la media y cuántas tienen una altura inferior a la media.
         // El valor de N se pide por teclado y debe ser entero positivo.
         Scanner sc = new Scanner(System.in);
-        int arrayLenght = sc.nextInt();
-        int[] arrayPerson = new int[arrayLenght];
 
-        for(int i = 0; i < arrayPerson.length; i++){
-            System.out.println("Indique por teclado el número de personas que desee");
-            arrayPerson[i] = sc.nextInt();
+        System.out.println("Indíqueme el número de personas");
+        int arrayLength = sc.nextInt();
+        int[] arrayPerson = new int[arrayLength];
+
+        for (int i = 0; i < arrayPerson.length; i++){
+            System.out.println("Indíqueme la altura de la persona en centímetros");
+            int heightPerson = sc.nextInt();
+            arrayPerson[i] = heightPerson;
         }
+
+        int sumNumArray = 0;
+
+        for (int a = 0; a < arrayPerson.length; a++) {
+            sumNumArray += arrayPerson[a];
+        }
+
+        int averageHeight = sumNumArray/arrayPerson.length;
+
+        int personHighAverage = 0;
+        int personLessAverage = 0;
+        for (int b = 0; b < arrayPerson.length; b++) {
+            if(arrayPerson[b] > averageHeight){
+                personHighAverage++;
+            } else if (arrayPerson[b] < averageHeight) {
+                personLessAverage++;
+            }
+        }
+
+        System.out.println("La altura media del número de personas es " + averageHeight);
+        System.out.println("Hay " + personHighAverage + " superior a la media");
+        System.out.println("Hay " + personLessAverage + " inferior a la media");
     }
 }
