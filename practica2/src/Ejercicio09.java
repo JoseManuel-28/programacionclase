c11bf4c907d998c9158a1c710fd25315959f7bf7
Kama <josemanuelmartinez.nene@gmail.com>
2025-10-21 18:35:14 +0200

    Hacer ejercicios 1-15

---
+++ practica2/src/Ejercicio09.java
@@ -0,0 +1,11 @@
+public class Ejercicio09 {
+    public static void main(String[] args){
+
+        //Crea un programa en Java que:
+        //Pida al usuario por teclado 10 números enteros y los guarde en un array.
+        //Recorra el array para encontrar:
+        //El valor máximo y la posición en la que aparece.
+        //El valor mínimo y la posición en la que aparece.
+        //Muestre el resultado en consola, por ejemplo:
+    }
+}
---
+++ practica2/src/Ejercicio11.java
@@ -0,0 +1,8 @@
+public class Ejercicio11 {
+    public static void main(String[] args){
+
+        //Escribe un programa java que invierta el orden de los valores de un array.
+        // Por invertir el orden de los valores de un array, me refiero que el último pasa a ser el primero,
+        // el penúltimo el segundo y así sucesivamente. PRUEBA CON UN ARRAY DE TAMAÑO 6.
+    }
+}
