0f406c993659eae91efdf9d96744bb32102de1cb
Jose Manuel <josemanuelmartinez.nene@gmail.com>
2025-10-21 11:34:43 +0200

    Ejercicios terminados

---
+++ practica2/src/Ejercicio07.java
@@ -0,0 +1,7 @@
+public class Ejercicio07 {
+    public static void main(String[] args){
+        //Programa Java que llene un array con 10 números enteros que se leen por teclado.
+        // A continuación calcula y muestra la media de los valores positivos
+        // y la dae los valores negativos del array.
+    }
+}
