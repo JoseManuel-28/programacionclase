983e5c5ca2a657fdaf2d637eb21113b40a22f327
Kama <josemanuelmartinez.nene@gmail.com>
2025-11-06 16:07:52 +0100

    ejercicio2 y ejercicio3 terminados

--- practica03/src/ejercicio3.java
+++ practica03/src/ejercicio3.java
@@ -1,29 +1,18 @@
-import java.util.Scanner;
+import java.util.Random;
 
-public class ejercicio2 {
-    public static void main(String[] args) {
-        /*Escribe un programa que solicite 20 números enteros. Estos números debemos de introducirlo en un array
-        de 4 filas por 5 columnas. El programa mostrará las sumas parciales de filas y en las columnas
-        el mayor número de la columna. La suma total debe aparecer en la esquina inferior derecha.*/
+public class ejercicio3 {
+    public static void main(String[] args){
 
-        /*Scanner sc = new Scanner(System.in);
-        int [][] arrayInt = new int[4][5];
 
-        for (int i = 0; i < arrayInt.length; i++) {
-            for (int a = 0; a < arrayInt.length; a++) {
-                System.out.println("indiqueme 1 número entero 20 veces");
-                arrayInt[i][a] = sc.nextInt();
-            }
-        }*/
-
-        int[][] arrayInt ={
-                {5, 7, 9, 10, 34},
-                {12, 45, 67, 18, 56},
-                {23, 57, 78, 12, 69},
-                {4, 8, 90, 46, 96}
-        };
         System.out.println("--------------------------------------------------------------------------------------------------");
+        int[][] arrayInt =new int[4][5];
+        Random arrayRand = new Random();
+        for (int i = 0; i <arrayInt.length; i++) {
+            for (int a = 0; a < arrayInt[i].length; a++) {
+                arrayInt[i][a] = arrayRand.nextInt(999) +1;
 
+            }
+        }
 
         int totalSum = 0;
 
@@ -50,12 +39,10 @@ public class ejercicio2 {
             System.out.printf("columna "  +  a  +": " + + columMayor + " ");
 
 
-            }
+        }
 
         System.out.println("  Suma total" + totalSum);
         System.out.println("--------------------------------------------------------------------------------------------------");
     }
 }
-
-
 
