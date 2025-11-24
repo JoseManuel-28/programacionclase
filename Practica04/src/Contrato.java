7815bc05b18b8662c71f5715e591125165fa782a
Jose Manuel <josemanuelmartinez.nene@gmail.com>
2025-11-24 14:26:24 +0100

    creacion de contrato

--- Practica04/src/Contrato.java
+++ Practica04/src/Contrato.java
@@ -1,7 +1,40 @@
 public class Contrato {
 
+
     //Atributos
-    public static int fechaDeCreacion;
-    public Medico medico;
-    public Hospital hospital;
+    private static int fechaDeCreacion;
+    private Medico medico;
+    private Hospital hospital;
+
+
+    //Constructor
+    public Contrato (int fc, Medico med, Hospital hos){
+        this.fechaDeCreacion = fc;
+        this.medico = med;
+        this.hospital = hos;
+    }
+
+
+    //Getter
+    public static int getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+    public Medico getMedico() {
+        return medico;
+    }
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+
+    //Setter
+    public static void setFechaDeCreacion(int fechaDeCreacion) {
+        Contrato.fechaDeCreacion = fechaDeCreacion;
+    }
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
 }
