2449558745e0e0828315d6cfff31b00cff1086e7
darkblade28 <josemanuelmartinez.nene@gmail.com>
2026-03-09 10:28:31 +0100

    cambio en el codigo en actuadores, codigo borrado por error

--- practica_08/src/actuadores.java
+++ practica_08/src/actuadores.java
@@ -1,16 +1,8 @@
-public class actuadores extends equipos{
+public class actuadores extends equipos {
     public sensores sensores;
 
     public actuadores(int numSerie, sensores sensores) {
         super(numSerie);
         this.sensores = sensores;
-    }
-
-    public void calentar(){
-        if(sensores.regitroTemperatura < sensores.get(nue))
-    }
-
-    public void regar(){
-
     }
 }
